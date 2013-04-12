241e18d4855c8e6c8a316328b50f6c14ff32a20f
PaigePonzeka <paigepon@gmail.com>
2013-04-12 09:38:53 -0400

    Added the intial files for signing in and signing up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f9689f81cb8767ed2b1fdada33c4a2686028dd0c213417e8dbd47ef710546687801534f3e7f54e65c6c15ca3ce4e4eca111ecbdb17e38cf2a621d924e097af42'
