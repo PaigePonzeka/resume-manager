ad3fdf8e08eb48cc805ba41dece3017bdee08489
Paige Ponzeka <paige.ponzeka@patch.com>
2013-04-16 20:37:31 -0400

    Attempt to fix the salt error on heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,7 +54,7 @@ class User < ActiveRecord::Base
    private
 
      def encrypt_password
-       self.salt = make_salt if new_record?
+       user.salt = make_salt if new_record?
        self.encrypted_password = encrypt(password)
      end
 
