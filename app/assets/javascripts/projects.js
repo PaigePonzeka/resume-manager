39a917f1674fd74275149b0025b2656f883244d0
Paige Ponzeka <paige.ponzeka@patch.com>
2013-04-19 14:23:07 -0400

    Adding JQuery to the app and gemfile, updating the layout of the projects form

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+console.log("rawr");
+$(document).ready(function(){
+	$('.screenshot').each(function(){
+		var url = $(this).find('.screenshot-url').val();
+		$(this).find('.screenshot-sample img').attr('src', url);
+	});
+});
